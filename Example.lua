a358232f1b1767de7c88cec176b86a973ecc6b54
joshyfr <127086240+joshyfr@users.noreply.github.com>
2023-05-21 16:55:34 +1000

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,28 +1,49 @@
--- Example of use of the UI Library
-
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/joshyfr/UI-Lib/main/UILib.lua"))();
-
-lib:SetTitle("Hub Name - Game")
-
-local tab1 = lib:NewTab("Automatic", "Automate Stuff")
-
-tab1:NewToggle("Auto click",false,function(value)
-   if value == true then
-        -- Start Autoclick
-        end
-    elseif value == false then
-        -- Stop autoclick
-    end
+local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
+local Window = UILib.new("Grand Piece Online", game.Players.LocalPlayer.UserId, "Buyer")
+local Category1 = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
+local SubButton1 = Category1:Button("Combat", "http://www.roblox.com/asset/?id=8395747586")
+local Section1 = SubButton1:Section("Section", "Left")
+Section1:Button({
+    Title = "Kill All",
+    ButtonName = "KILL!!",
+    Description = "kills everyone",
+    }, function(value)
+    print(value)
 end)
-
-tab1:NewSlider("Walkspeed",16,250,16,function(value)
-   game:GetService("Workspace").LocalPlayer.Humanoid.Walkspeed = value;
+Section1:Toggle({
+    Title = "Auto Farm Coins",
+    Description = "Optional Description here",
+    Default = false
+    }, function(value)
+    print(value)
 end)
-
-tab1:NewDropdown("Noob Selector","Another Noob","Default Noob",function(value)
-    Noob = value;
+Section1:Slider({
+    Title = "Walkspeed",
+    Description = "",
+    Default = 16,
+    Min = 0,
+    Max = 120
+    }, function(value)
+    print(value)
 end)
-
-tab1:NewTextBox("Textbox","Placeholder Text",function(textput)
-  CustomNoob = textput;
+Section1:ColorPicker({
+    Title = "Colorpicker",
+    Description = "",
+    Default = Color3.new(255,0,0),
+    }, function(value)
+    print(value)
+end)
+Section1:Textbox({
+    Title = "Damage Multiplier",
+    Description = "",
+    Default = "",
+    }, function(value)
+    print(value)
+end)
+Section1:Keybind({
+    Title = "Kill All",
+    Description = "",
+    Default = Enum.KeyCode.Q,
+    }, function(value)
+    print(value)
 end)
